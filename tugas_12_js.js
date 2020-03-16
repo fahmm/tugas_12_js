81e160da45e00c377d7b2e5f42a6403a351d233e
Fahmi <pammimajid@gmail.com>
2020-03-16 20:48:37 +0800

    okk

---
+++ tugas_12_js.js
@@ -0,0 +1,11 @@
+function panggilbuah(){
+	var buah = ['pisang','jeruk','apel','mangga']
+	console.log(buah);
+	var buah2 = buah.shift();
+	console.log(buah);
+	var buah3 = buah.pop();
+	return buah
+
+}
+
+console.log(panggilbuah());
